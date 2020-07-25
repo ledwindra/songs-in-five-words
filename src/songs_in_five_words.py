f082363d1967e513fb6b7312c763246f62b53f0e
ledwindra <lukman.edwindra@gmail.com>
2020-07-25 17:10:47 +0700

    modified script so that the visualizating uses XKCD style

--- src/songs_in_five_words.py
+++ src/songs_in_five_words.py
@@ -1,7 +1,9 @@
 import argparse
 import lyricsgenius
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
 import pandas as pd
+import re
 
 class Lyrics:
     
@@ -10,7 +12,6 @@ class Lyrics:
         self.title = args.title
         self.artist = args.artist
         self.vanilla = args.vanilla
-        genius = lyricsgenius.Genius('my_client_access_token_here')
         
     def song(self):
         genius = lyricsgenius.Genius(self.token)
@@ -21,21 +22,30 @@ class Lyrics:
         lyrics = self.song()
         lyrics = lyrics.split("\n")
         lyrics = [x.replace(",", "") for x in lyrics]
-        lyrics = [x.lower() for x in lyrics if "[" not in x]        
         lyrics = " ".join(lyrics).split(" ")
+        lyrics = [re.sub(r'[^\w]', '', x.lower()) for x in lyrics]
         if not self.vanilla:
             remove = [
                 'a',
+                'as',
                 'and',
                 'by',
+                'dont',
                 'i',
                 'it',
+                'its',
+                'me',
+                'my',
+                'of',
                 'oh',
                 'or',
+                'some',
                 'that',
                 'the',
                 'this',
                 'to',
+                'we',
+                'with',
                 'yeah',
                 'you'
             ]
@@ -44,20 +54,42 @@ class Lyrics:
         unique = [x for x in unique if x != '']
         df = pd.DataFrame([[x, lyrics.count(x)] for x in unique], columns=['words', 'count'])
         df = df.sort_values(by = 'count', ascending=False)
-        top = df.iloc[:4]
-        top = top.append(pd.DataFrame([{'words': 'others', 'count': sum(df.iloc[4:,1])}]))
+        top = df.iloc[:5]
+        top = top.append(pd.DataFrame([{'words': 'others', 'count': sum(df.iloc[5:,1])}]))
         top = top.reset_index(drop=True)
         
         return top
 
     def visualize(self, top):
+        font = {
+            'fontname':'Comic Sans MS',
+            'size': 22
+        }
+        legend_font = font_manager.FontProperties(
+            family='Comic Sans MS',
+            weight='bold',
+            style='normal',
+            size=16
+        )
+        plt.xkcd()
         labels = top.iloc[:,0] 
         sizes = top.iloc[:,1]
-        colors = ['black', 'grey', 'darkgrey', 'lightgrey', 'whitesmoke']
-        patches, texts = plt.pie(sizes, colors=colors, startangle=90)
-        plt.legend(patches, labels, loc='best')
+        colors = ['black', 'grey', 'darkgrey', 'lightgrey', 'gainsboro', 'whitesmoke']
+        patches, texts = plt.pie(
+            sizes,
+            colors=colors,
+            startangle=90,
+            wedgeprops={
+                'edgecolor':'k',
+                'linewidth': 1,
+                'linestyle': 'solid'
+            }
+        )
+        plt.title(self.title,  **font)
+        plt.legend(patches, labels, loc='best', prop=legend_font)
         plt.axis('equal')
         plt.tight_layout()
+        plt.xkcd()
         plt.show()
         
 if __name__ == '__main__':
